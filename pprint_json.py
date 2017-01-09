ca4f9e79cc3182762b4f62b1978385dcfd5eee00
Andrey <ibleminov@yandex.ru>
2017-01-09 16:47:48 +0300

    some changes

--- pprint_json.py
+++ pprint_json.py
@@ -3,14 +3,24 @@ import json
 
 def load_data(filepath):
     with open(filepath,'r',encoding='utf-8') as json_to_parse:
-        json_to_parse = json.loads(json_to_parse.read())
-    return json_to_parse
+        return json.load(json_to_parse)
+
 
 
 def pretty_print_json(data):
-    data = json.dumps(data,sort_keys=True,indent=4,ensure_ascii=False)
-    return data
+    print (json.dumps(data,sort_keys=True,indent=4,ensure_ascii=False))
+
 
 if __name__ == '__main__':
-    json_content = load_data(input("Enter json-file path: "))
-    print ('Pretty print: \n', pretty_print_json(json_content))
+    while True:
+        filepath = input('Enter filepath for json file : ')
+        if not filepath:
+            print('Try again!')
+        else:
+            try:
+                pretty_print_json(load_data(filepath))
+                break
+            except FileNotFoundError as e:
+                print("Try better : %s" % (e.args[1]))
+
+
