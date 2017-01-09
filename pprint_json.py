d19d3c6ec6422a4446445e93777d3917206c0ebe
Andrey <ibleminov@yandex.ru>
2017-01-09 15:28:39 +0300

    update script

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath,'r',encoding='utf-8') as json_to_parse:
+        json_to_parse = json.loads(json_to_parse.read())
+    return json_to_parse
 
 
 def pretty_print_json(data):
-    pass
-
+    data = json.dumps(data,sort_keys=True,indent=4,ensure_ascii=False)
+    return data
 
 if __name__ == '__main__':
-    pass
+    json_content = load_data(input("Enter json-file path: "))
+    print ('Pretty print: \n', pretty_print_json(json_content))
