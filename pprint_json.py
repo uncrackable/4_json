9daed1f8c2218b6c303202301ef12266e8dbd0c0
Andrey <ibleminov@yandex.ru>
2017-01-13 15:03:38 +0300

    led to PEP8 + some changes

--- pprint_json.py
+++ pprint_json.py
@@ -2,22 +2,23 @@ import json
 import os
 import argparse
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
-        return None
-    with open(filepath,'r',encoding='utf-8') as json_file:
+        return 'No such file in directory'
+    with open(filepath, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
 
-
 def pretty_print_json(data):
-    print (json.dumps(data,sort_keys=True,indent=4,
-           ensure_ascii=False))
+    return json.dumps(data, sort_keys=True, indent=4,
+                      ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='...')
-    parser.add_argument('path', help='Enter filepath to your JSON')
-    arg = parser.parse_args()
-    json_content = load_data(arg.path)
-pretty_print_json(json_content)
+    parser = argparse.ArgumentParser(description='JSON pretty print script')
+    parser.add_argument('-f', '--file', required='True',
+                        help='filepath to your JSON')
+    args = parser.parse_args()
+    json_content = load_data(args.file)
+    print(pretty_print_json(json_content))
